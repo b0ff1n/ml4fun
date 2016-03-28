b845e159eb3563109705a46c7b67ed8962dce0be
b0ff1n <eugene@amobee.com>
2016-03-28 16:37:54 +0300

    assignment step

--- src/clj/ml4fun/k_means.clj
+++ src/clj/ml4fun/k_means.clj
@@ -2,13 +2,27 @@
   (:require [clojure.pprint :refer [pprint]]
             [clojure.tools.reader.edn :as edn]))
 
-(defn dist- [[x1 y1] [x2 y2]]
+(defn dist [[x1 y1] [x2 y2]]
   (+ (* (- x1 x2) (- x1 x2))
      (* (- y1 y2) (- y1 y2))))
 
+(defn nearest-center [centers n]
+  (let [dist-from (partial dist n)]
+    (apply min-key dist-from centers)))
+
+
+
+
 
 (defn k-means [input centers]
-  (count input))
+  (group-by (partial nearest-center centers) input))
 
 
 
+
+
+;; (def centers [[10 1] [10 2] [11 1] [11 2]])
+;; (def input (user/input :k-means))
+
+
+;; (def s1 (k-means input centers))
